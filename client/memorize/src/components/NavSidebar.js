652694039b4401a1d5141feff820a8e1be4b9a53
Sean Nordquist <srn334@nyu.edu>
2020-07-18 15:28:17 -0700

    created static browse page

--- client/memorize/src/components/NavSidebar.js
+++ client/memorize/src/components/NavSidebar.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-
 
 class NavSidebar extends Component {
     render() {
         return(
-            <div>
-                <Nav vertical>
-                    <NavItem>
-                        <NavLink disabled href="#">Create</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink disabled href="#">Study</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink disabled href="#">Browse</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink disabled href="#">Settings</NavLink>
-                    </NavItem>
-                </Nav>
-            </div>
+            <Nav vertical>
+                <NavItem>
+                    <NavLink disabled href="#">Create</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink disabled href="#">Study</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink disabled href="#">Browse</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink disabled href="#">Settings</NavLink>
+                </NavItem>
+            </Nav>
+
         );
     }
 }
