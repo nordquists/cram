4f40aa72f9eed7788c61b4524d44b1c51310f212
Sean Nordquist <srn334@nyu.edu>
2020-08-20 22:58:29 -0700

    added new redirect uri for domain

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ const env = process.env.NODE_ENV; // current environment
 // axios.defaults.baseURL = window.location.host;
 
 const redirectUri = env === 'production' 
-                          ? 'https://usecram.herokuapp.com/check-login'
+                          ? 'https://usecram.com/check-login'
                           : 'http://localhost:3000/check-login'
 
 const App = () => {
