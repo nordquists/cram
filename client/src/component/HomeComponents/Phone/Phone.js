b7e7682f464d70fc119b4cfafa6e8e77932ee26d
Sean Nordquist <srn334@nyu.edu>
2020-08-23 00:21:25 -0700

    bug fixes

--- client/src/component/HomeComponents/Phone/Phone.js
+++ client/src/component/HomeComponents/Phone/Phone.js
@@ -31,7 +31,7 @@ export const Phone = () => {
 
             <ReactPlayer
                     className='react-player'
-                    url='/cramDemoHD.mp4'
+                    url='/CramDemoHD.mp4'
                     width='100%'
                     height='100%'
                     muted={true}
